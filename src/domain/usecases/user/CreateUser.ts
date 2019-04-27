35aea27bc9f2fce6903c6aa9d78808ba22d9e0fc
leolana <leolucas.lana@hotmail.com>
2019-04-27 10:45:58 -0300

    feat: create signup model as input of mutation

--- src/domain/usecases/user/CreateUser.ts
+++ src/domain/usecases/user/CreateUser.ts
@@ -7,7 +7,7 @@ import {
     EventDispatcher, EventDispatcherInterface
 } from '../../../infra/decorators/EventDispatcher';
 import { Logger, LoggerInterface } from '../../../infra/decorators/Logger';
-import { User } from '../../entities/User';
+// import { User } from '../../entities/User';
 import { events } from '../../subscribers/events';
 
 @Service()
@@ -18,7 +18,7 @@ export class CreateUser {
     @Logger(__filename) private logger: LoggerInterface
   ) {}
 
-  public async execute(user: User): Promise<IUserModel> {
+  public async execute(user: any): Promise<IUserModel> {
     this.logger.info('Create a new user => ', user.toString());
     user.id = uuid.v4();
     const newUser = await this.userModel.create(user);
--- src/interfaces/graphql/resolvers/AccountResolver.ts
+++ src/interfaces/graphql/resolvers/AccountResolver.ts
@@ -2,14 +2,16 @@ import { Mutation, Resolver, Root } from 'type-graphql';
 import { Service } from 'typedi';
 
 import { CreateUser } from '../../../domain/usecases/user/CreateUser';
+import { SignupAccountInput } from '../types/input/SignupAccountInput';
+import { User } from '../types/User';
 
 @Service()
 @Resolver()
 export class AccountResolver {
   constructor(private createUserUseCase: CreateUser) {}
 
-  @Mutation(returns => String)
-  public async signup(@Root() user: UserModel): Promise<any> {
-    return this.createUserUseCase.execute(user);
+  @Mutation(returns => User)
+  public async signup(@Root() signUpAccount: SignupAccountInput): Promise<any> {
+    return this.createUserUseCase.execute(signUpAccount);
   }
 }
