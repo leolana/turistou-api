2c157ba0c8dee5fc10d5fd4685534eae8451cb21
leolana <leolucas.lana@hotmail.com>
2019-06-30 12:56:17 -0300

    feat: add schema to user and customer

--- src/infra/database/schemas/userSchema.ts
+++ src/infra/database/schemas/userSchema.ts
@@ -4,20 +4,181 @@ import { IUser } from '@domain/entities/IUser';
 
 import { DbSchema } from './DbSchema';
 
+const dataTypes = mongoose.Schema.Types;
+
 export interface IUserModel extends IUser, mongoose.Document {
   fullName(): string;
 }
 
+export enum Gender {
+  Male = 'MASC',
+  Female = 'FEM'
+}
+
+export enum Identity {
+  Person = 'PF',
+  Company = 'PJ'
+}
+
+const stateSchema: mongoose.Schema = new mongoose.Schema(
+  {
+    initials: {
+      type: dataTypes.String,
+      required: true,
+      maxlength: 2,
+    },
+    name: {
+      type: dataTypes.String,
+      required: true,
+      maxlength: 30
+    },
+  },
+);
+
+const citySchema: mongoose.Schema = new mongoose.Schema(
+  {
+    name: {
+      type: dataTypes.String,
+      required: true,
+      maxlength: 30
+    },
+    state: {
+      type: dataTypes.String,
+      maxlength: 2
+    },
+  },
+);
+
+const occupationType: mongoose.Schema = new mongoose.Schema(
+  {
+    name: {
+      type: dataTypes.String,
+      required: true,
+      maxlength: 30
+    },
+  },
+);
+
+const companyType: mongoose.Schema = new mongoose.Schema(
+  {
+    name: {
+      type: dataTypes.String,
+      required: true,
+      maxlength: 30
+    },
+  },
+);
+
 const userSchema: mongoose.Schema = new mongoose.Schema(
   {
-    email: mongoose.Schema.Types.String,
-    firstName: mongoose.Schema.Types.String,
-    lastName: mongoose.Schema.Types.String,
-    password: mongoose.Schema.Types.String,
+    email: {
+      type: dataTypes.String,
+      required: true,
+      maxlength: 255,
+    },
+    name: {
+      type: dataTypes.String,
+      required: true,
+      maxlength: 30
+    },
+    lastName: {
+      type: dataTypes.String,
+      maxlength: 50
+    },
+    phone: {
+      type: dataTypes.String,
+      maxlength: 11,
+      required: true
+    },
+    cpf: {
+      type: dataTypes.String,
+      maxlength: 11,
+    },
+    gender: {
+      type: dataTypes.String,
+    },
+    birthDate: {
+      type: dataTypes.Date,
+    },
     active: {
-      type: mongoose.Schema.Types.Boolean,
+      type: dataTypes.Boolean,
       default: true,
-    }
+    },
+  },
+  { timestamps: true }
+);
+
+const customerSchema: mongoose.Schema = new mongoose.Schema(
+  {
+    address: {
+      type: dataTypes.String,
+      maxlength: 150,
+    },
+    zipcode: {
+      type: dataTypes.String,
+      maxlength: 8,
+    },
+    area: {
+      type: dataTypes.String,
+      maxlength: 150,
+    },
+    number: {
+      type: dataTypes.String,
+      maxlength: 30
+    },
+    complement: {
+      type: dataTypes.String,
+      maxlength: 30
+    },
+    state: {
+      type: dataTypes.String,
+      maxlength: 2
+    },
+    city: {
+      type: dataTypes.String,
+      maxlength: 150
+    },
+    cadastur: {
+      type: dataTypes.String,
+      maxlength: 11
+    },
+    cadasturExpiration: {
+      type: dataTypes.Date,
+    },
+    identityType: {
+      type: dataTypes.String,
+      maxlength: 2,
+    },
+    occupationType: {
+      type: dataTypes.ObjectId,
+    },
+    occupationTypeCustom: {
+      type: dataTypes.String,
+      maxlength: 30,
+    },
+    companyType: {
+      type: dataTypes.ObjectId,
+    },
+    companyTypeCustom: {
+      type: dataTypes.String,
+      maxlength: 30,
+    },
+    companyName: {
+      type: dataTypes.String,
+      maxlength: 200,
+    },
+    companyTradeName: {
+      type: dataTypes.String,
+      maxlength: 200,
+    },
+    cnpj: {
+      type: dataTypes.String,
+      maxlength: 14,
+    },
+    active: {
+      type: dataTypes.Boolean,
+      default: true,
+    },
   },
   { timestamps: true }
 );
