1c71570e7419b5ed1f75fe3cf409a41e0ce4caf1
Leonardo Lana <leonardo.lana@itlab.com.br>
2019-11-02 11:27:03 -0300

    feat: create endpoint to excursion

--- src/interfaces/graphql/resolvers/ExcursionResolver.ts
+++ src/interfaces/graphql/resolvers/ExcursionResolver.ts
@@ -1,7 +1,9 @@
-import { Query, Resolver } from 'type-graphql';
+// import { Authorized, Mutation, Query, Resolver } from 'type-graphql';
+import { Authorized, Query, Resolver } from 'type-graphql';
 import { Service } from 'typedi';
 
 import ListExcursion from '@domain/usecases/excursion/ListExcursion';
+// import { entityToExcursionSerializer, inputToExcursionModel } from '@interfaces/mapper/ExcursionMapper';
 import { entityToExcursionSerializer } from '@interfaces/mapper/ExcursionMapper';
 
 import { Excursion } from '../types/Excursion';
@@ -11,10 +13,18 @@ import { Excursion } from '../types/Excursion';
 export class ExcursionResolver {
   constructor(private listExcursionsUseCase: ListExcursion) {}
 
-  // @Authorized()
+  @Authorized()
   @Query(returns => [Excursion])
   public async excursions(): Promise<Excursion[]> {
     const excursions = await this.listExcursionsUseCase.execute({});
     return excursions.map(entityToExcursionSerializer);
   }
+
+  // @Authorized()
+  // @Mutation(returns => Excursion)
+  // public async saveExcursion(input): Promise<Excursion> {
+  //   const excursionInput = inputToExcursionModel(input);
+  //   const excursion = await this.listExcursionsUseCase.execute(excursionInput);
+  //   return entityToExcursionSerializer(excursion);
+  // }
 }
