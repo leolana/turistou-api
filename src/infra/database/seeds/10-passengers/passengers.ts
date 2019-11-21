8302bde60b1faa1801a298b84f2db6c9b3d0e50b
Breno <breno_asm@hotmail.com>
2019-11-21 20:57:50 -0300

    Structured the seeds

--- src/infra/database/seeds/10-passengers/passengers.ts
+++ src/infra/database/seeds/10-passengers/passengers.ts
@@ -6,6 +6,19 @@ enum PassengerStatus {
   canceled = 'CANCELED'
 }
 
+enum OperationPayment {
+  Credit = 'CREDIT',
+  ChargeBack = 'CHARGE_BACK',
+}
+
+// id: String;
+// value: Number;
+// dueDate: Date;
+// payDate: Date;
+// operation: OperationPayment;
+// createdAt: Date;
+// updatedAt: Date;
+
 export = [
   {
     customerId: new ObjectId('5d5821a9ffc3c7010f0c2f22'),
@@ -14,6 +27,24 @@ export = [
     // ticketPriceId: new ObjectId(''),
     spot: 1,
     // transportExcursionId: new ObjectId(''),
+    payments: [
+      {
+        value: 60,
+        dueDate: new Date(),
+        payDate: new Date(),
+        operation: OperationPayment.ChargeBack,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        value: 30,
+        dueDate: new Date(),
+        payDate: new Date(),
+        operation: OperationPayment.Credit,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ]
   },
   {
     customerId: new ObjectId('5d5821a9ffc3c7010f0c2f23'),
@@ -22,6 +53,7 @@ export = [
     // ticketPriceId: new ObjectId(''),
     spot: 2,
     // transportExcursionId: new ObjectId(''),
+    payments: []
   },
   {
     customerId: new ObjectId('5d5821a9ffc3c7010f0c2f24'),
@@ -30,6 +62,16 @@ export = [
     // ticketPriceId: new ObjectId(''),
     spot: 15,
     // transportExcursionId: new ObjectId(''),
+    payments: [
+      {
+        value: 45,
+        dueDate: new Date(),
+        payDate: new Date(),
+        operation: OperationPayment.Credit,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ]
   },
   {
     customerId: new ObjectId('5d5821a9ffc3c7010f0c2f25'),
@@ -38,5 +80,23 @@ export = [
     // ticketPriceId: new ObjectId(''),
     spot: 18,
     // transportExcursionId: new ObjectId(''),
+    payments: [
+      {
+        value: 20,
+        dueDate: new Date(),
+        payDate: new Date(),
+        operation: OperationPayment.ChargeBack,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        value: 80,
+        dueDate: new Date(),
+        payDate: new Date(),
+        operation: OperationPayment.ChargeBack,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ]
   },
 ];
