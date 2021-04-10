63ef55406f08dd384767fcd227e4dfa6ddce3bc9
MTRecouso <mateus.recouso@hotmail.com>
2021-04-10 20:23:32 -0300

    feat: removed unecessary id on PaymentCondition

--- src/interfaces/graphql/types/PaymentCondition.ts
+++ src/interfaces/graphql/types/PaymentCondition.ts
@@ -1,4 +1,4 @@
-import { Field, Float, ID, ObjectType } from 'type-graphql';
+import { Field, Float, ObjectType } from 'type-graphql';
 
 import { PaymentTypes } from '@domain/entities/PaymentCondition';
 
